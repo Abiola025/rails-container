b98acc518354e9ac9ac60cbf470ca5de5fe8bfdf
Thomas Jamshidnezhad thomas.jamshidnezhad@gmail.com <thomas.jamshidnezhad@gmail.com>
2025-05-20 16:49:37 +0100

    created new and create actions, created new view in container

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,5 +1,30 @@
 class ContainersController < ApplicationController
+  before_action :set_user, only: %i[create]
   def index
     @containers = Container.all
   end
+  def new
+    @container = Container.new
+  end
+
+  def create
+    @container = Container.new(container_params)
+    @container.user = @user
+    if @container.save
+      redirect_to containers_path
+    else
+      render :new, status: :unprocessable_entity
+  end
+end
+
+private
+
+def container_params
+  params.require(:container).permit(:name, :detail, :width, :height, :length, :maximum_weight, :empty_weight, :daily_price, :image_url)
+end
+
+def set_user
+  @user = current_user
+end
+
 end
