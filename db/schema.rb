b219df75a809dcbcccaf9a3f8e5a302d8ae3423a
Michael McLaughlin <mmclaug@hotmail.co.uk>
2025-05-20 17:32:39 +0100

    started navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_20_114515) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_20_115246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.text "comment"
+    t.date "date_from"
+    t.date "date_to"
+    t.decimal "price"
+    t.boolean "accept"
+    t.boolean "decline"
+    t.bigint "container_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["container_id"], name: "index_bookings_on_container_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "containers", force: :cascade do |t|
     t.string "name"
@@ -42,5 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_20_114515) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "containers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "containers", "users"
 end
