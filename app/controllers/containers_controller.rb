a971f912c89def0d6b5103ebb7cfef1e56050a72
Thomas Jamshidnezhad thomas.jamshidnezhad@gmail.com <thomas.jamshidnezhad@gmail.com>
2025-05-23 15:03:45 +0100

    pre demo changes

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,5 +1,6 @@
 class ContainersController < ApplicationController
   before_action :set_user, only: %i[new create]
+  skip_before_action :authenticate_user!, only: :index
   def index
     @containers = Container.all
   end
