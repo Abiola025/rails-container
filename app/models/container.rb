e4383d14d3c2bf31d344820388ec4fb7babc504c
Abiola Oladunni <abiolaola85@gmail.com>
2025-05-23 15:54:22 +0100

    testing

--- app/models/container.rb
+++ app/models/container.rb
@@ -3,7 +3,7 @@ class Container < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
-  validates :photo, presence: true
+
   validates :name, presence: true, uniqueness: {scope: :user_id, message: "You have already created a container with this name"}
   validates :detail, presence: true, length: { minimum: 15 }
   validates :height,:width,:length, presence: true, format: { with: /\A\d+\.?\d{0,2}\z/, message: "enter to Maximum of 2 decimal places e.g. 1.25"}, numericality: {greater_than_or_equal_to: 1}
